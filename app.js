d29596b454bb8b0ecd0d9d6ed061a4a33bfee399
yaya <yaya@coolcto.com>
2016-08-20 21:45:28 +0800

    update package.json

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var ejs = require('ejs');
-var fs = require('fs');
-var routes = require('./routes/index.js');
-var users = require('./routes/user.js');
-var mew_util = require('mew_util');
-var app = express();
-var cors = require('cors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const fs = require('fs');
+const logger = require('morgan');
+const mew_util = require('mew_util');
+const cors = require('cors');
+const session = require('express-session');
+const methodOverride = require('method-override');
 
-// view engine setup
+const routes = require('./routes/index.js');
+const users = require('./routes/user.js');
+
+const config = require('./config.json');
+
+const app = express();
+
 app.engine('.html', ejs.__express);
 app.set('view engine', 'html');
 app.use(logger('dev'));
-app.use(express.bodyParser({
-	uploadDir: './uploads'
-}));
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('sctalk admin manager'));
-app.use(express.session());
+app.use(bodyParser.json({
+	"limit": "10mb"
+}));
+app.use(bodyParser.urlencoded({
+	"limit": "10mb",
+	"extended": false
+}));
+app.use(methodOverride());
+app.use(cookieParser('sctalk admin manager'));
+app.use(session({
+	secret: 'niconiconi',
+	resave: false,
+	saveUninitialized: true,
+	cookie: { secure: true }
+}));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(app.router);
 
 app.get('/admin', routes.admin);
 app.get('/admin/article*', routes.article);
@@ -35,28 +44,16 @@ app.get('/admin/newarticle', routes.newarticle);
 app.get("/admin/getUploadToken", routes.getUploadToken);
 app.post("/admin/saveArticle", routes.saveArticle);
 
-// catch 404 and forwarding to error handler
+
 app.use(function(req, res, next) {
 	res.status(404);
 	res.end();
 });
-/// error handlers
-
-//development error handler
-//will print stacktrace
-// app.use(function(err, req, res, next) {
-// 	res.json({
-// 		err: true,
-// 		msg: '服务器出错',
-// 		result: null
-// 	});
-// });
 
 mew_util.async(function() {
 	routes.init(this.next);
 }).then(function() {
 	app.listen(config.serverRuntime.port);
-	console.log("server try to listen on " + port);
+	console.log("server try to listen on " + config.serverRuntime.port);
 	console.log('server started');
 })
-console.log('server started');
